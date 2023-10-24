f6e2d827053c0fd512994e9cad7e238703f2909e
Louis <louisapstg@gmail.com>
2023-10-24 22:05:16 +0700

    Updates

--- src/assets/index.js
+++
@@ -1,8 +0,0 @@
-import Briefcase from "./Briefcase.svg";
-import Send from "./Send.svg";
-import Wifi from "./Wifi.svg";
-import Trello from "./Trello.svg";
-import Gift from "./Gift.svg";
-import Globe from "./Globe.svg";
-
-export { Briefcase, Send, Wifi, Trello, Gift, Globe }
